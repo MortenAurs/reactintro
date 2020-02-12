f11d9cde5d4a9d0e2067349484bd1244b0bf62af
Morten Aursland <morten.aursland@gmail.com>
2020-02-12 18:13:42 +0100

    Removed function and some imports

--- src/Components/svgHandler.js
+++ src/Components/svgHandler.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import {UncontrolledReactSVGPanZoom} from 'react-svg-pan-zoom';
-import {ReactSvgPanZoomLoader} from 'react-svg-pan-zoom-loader'
+import {
+   fitSelection,
+   fitToViewer,
+   INITIAL_VALUE,
+   ReactSVGPanZoom,
+   TOOL_PAN,
+   zoomOnViewerCenter
+ } from 'react-svg-pan-zoom';
+ import {ReactSvgPanZoomLoader} from 'react-svg-pan-zoom-loader'
 
 class NewToolbar extends React.Component{
-    render(){
-        return <span></span>
+    render(){ 
+       return <span></span>
     }
 }
 
@@ -14,30 +21,76 @@ class NewNavigation extends React.Component{
     }
 }
 export default class SvgHAndler extends React.PureComponent {
+   state = {tool: TOOL_PAN, 
+            value: INITIAL_VALUE, 
+            img_url: "floorplan.svg",
+         }
+   
 
-  Viewer = null
+   Viewer = null
 
-  componentDidMount() {
-    this.Viewer.fitToViewer();
+  
+   changeTool(nextTool) {
+      console.log("NEXTTOOL")
+    this.setState({tool: nextTool})
   }
 
+  changeValue(nextValue) {
+     console.log("NEXTVALUE:")
+     console.log(nextValue)
+     this.setState({value: nextValue})//, img_url: "cake.svg"})
+ }
+
+ changeFloor(floor) {
+   switch(floor){
+      case 1:
+         this.setState({img_url: "floorplan.svg"})
+         break;
+      case 2:
+         this.setState({img_url: "cake.svg"})
+         break;
+      default:
+         this.setState({img_url: "dd"})
+   } 
+}
+
+fitToViewer() {
+   this.setState(state => ({value: fitToViewer(state.value)}))
+ }
+
+ 
+
+
+
+ 
   render() {
+   console.log("THISSTATEVALUE")
+   console.log(this.state.value)
+   
     return (
       <div>
+        <button className="btn" onClick={() => this.changeFloor(1)}>1 etg</button>
+        <button className="btn" onClick={() => this.changeFloor(2)}>2 etg</button>
+        <button className="btn" onClick={() => this.fitToViewer()}>Tilpass vindu</button>
+        <hr/>
+        
          <ReactSvgPanZoomLoader 
-            src="cake.svg" 
+            src={this.state.img_url} 
             render= {(content) => (
-               <UncontrolledReactSVGPanZoom 
-                  width={500}
+               <ReactSVGPanZoom 
+               
+                  width={1000}
                   height={500}
-                  ref = {ref => this.Viewer = ref}
+                  tool={this.state.tool} onChangeTool={tool=>this.changeTool(tool)}
+                  value={this.state.value} onChangeValue={value => this.changeValue(value)}
                   customToolbar={NewToolbar} // Removes toolbar
                   customMiniature={NewNavigation} // Removes miniature map
                >
-                  <svg width={500} height={500} >
+                  <svg width={1000} height={500} >
                      {content}
                   </svg>  
-               </UncontrolledReactSVGPanZoom>
+                  
+               </ReactSVGPanZoom>
             )}
          />
       </div>
