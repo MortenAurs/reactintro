2dc7866565291191b2386957ee64f1f2c63ba422
Morten Aursland <morten.aursland@gmail.com>
2020-02-16 18:22:23 +0100

    added filterclass component

---
+++ src/Components/filtersearch.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class FilterSearch extends React.Component{
+   render(){
+      return (
+      <div>
+      
+      </div>
+      )
+  }
+}
+
+export default FilterSearch;
