6112ad9118c0ff404c61e9375a4405dff6b57ace
Morten Aursland <morten.aursland@gmail.com>
2020-02-16 15:40:15 +0100

    Added routing to project

--- src/Components/map.js
+++ src/Components/map.js
@@ -1,6 +1,7 @@
 import React from "react";
+import SvgHandler from "./svgHandler";
 
-class Welcome extends React.Component{
+class Map extends React.Component{
    constructor(props){
       super(props);
       this.state = {name: "Morten"}
@@ -9,11 +10,11 @@ class Welcome extends React.Component{
       return (
       <div>
          <h1>Hello, { this.state.name }</h1>
-         <p>This is a welcome message</p>
+         <p>Choose the floor</p>
+         <SvgHandler />
       </div>
       )
-      
   }
 }
 
-export default Welcome;
+export default Map;
--- src/app.js
+++ src/app.js
@@ -1,21 +1,53 @@
 import React from "react";
+import Home from "./Components/home";
 import Search from "./Components/search";
-import Welcome from "./Components/welcome";
+import Map from "./Components/map";
 import SvgHandler from "./Components/svgHandler";
 import FetchFromAPI from "./Components/fetchFromAPI";
+import {
+   BrowserRouter as Router,
+   Switch,
+   Route,
+   Link
+ } from "react-router-dom";
 
 
 class App extends React.Component{
    render(){
       return (
-      <div>
-         <Welcome />
-         <Search />
-         <SvgHandler />
-         <FetchFromAPI />
-      </div>
-      )
-  }
+         <Router>
+            <div>
+               <nav>
+                  <ul>
+                     <li>
+                     <Link to="/">Home</Link>
+                     </li>
+                     <li>
+                     <Link to="/Map">Map</Link>
+                     </li>
+                     <li>
+                     <Link to="/search">Search</Link>
+                     </li>
+                  </ul>
+               </nav>
+
+               <Switch>
+                  <Route path="/search">
+                     <Search />
+                  </Route>
+                  <Route path="/Map">
+                     <Map />
+                  </Route>
+                  <Route path="/">
+                     <Home />
+                  </Route>
+               </Switch>
+            </div>
+         </Router>
+      )};
 }
 
+
+    
+      
 export default App;
