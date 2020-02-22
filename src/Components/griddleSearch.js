35199d2b62af9d53b96fbc4b79f7556196408a65
Morten Aursland <morten.aursland@gmail.com>
2020-02-22 21:13:48 +0100

    Included some styling

--- src/Components/griddleSearch.js
+++ src/Components/griddleSearch.js
@@ -1,14 +1,48 @@
 import React from "react";
-import Griddle, {plugins} from "griddle-react";
+import Griddle, {plugins, RowDefinition, ColumnDefinition} from "griddle-react";
+
+const data = [
+   {
+     "id": 0,
+     "name": "Mayer Leonard",
+     "location": {
+       "country": "United Kingdom",
+       "city": "Kapowsin",
+       "state": "Hawaii",
+     },
+     "company": "Ovolo",
+     "favoriteNumber": 7
+   }];
+
+const CustomColumn = ({value}) => <span style={ {color: "yellow", backgroundColor: "blue"} }>{value}</span>;
+const CustomHeading = ({title}) => <span style={{ color: '#AA0000' }}>{title}</span>;
+const styleConfig = {
+   styles: {
+     Filter: { fontSize: 18 },
+     Table: { border: "2px solid #555 "},
+     TableBody: { 
+        textAlign: "center",
+        border: "1px solid #555 "
+      }
+   }
+ }
+
 
 class GriddleSearch extends React.Component{
+   
    render(){
+      console.log(data);
       return (
       <div>
          <Griddle
-               data={this.props.rooms}
-               plugins={[plugins.LocalPlugin]}
-            />
+            data={data}
+            plugins={[plugins.LocalPlugin]}
+         >
+           <RowDefinition>
+               <ColumnDefinition id="name" />
+               <ColumnDefinition id="location.state" title="State" />
+            </RowDefinition>
+      </Griddle>
       </div>
       )
   }
